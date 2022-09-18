b8a3d9965d9cd4c670411ebe67ad28bef2b2868e
Dennborg <mullerdennis1208@gmail.com>
2022-09-18 21:39:43 +0200

    super_pong.p8 -> animation and sprites + draw_sprites.lua -> animation

--- draw_sprites.lua
+++ draw_sprites.lua
@@ -4,4 +4,13 @@ sspr(16,0,2,14,126, enemy.y)
   spr(3,ball.x,ball.y)
 --  rect(hitboxp.x1, hitboxp.y1, hitboxp.x2, hitboxp.y2, 3)
 --  rect(hitboxe.x1, hitboxe.y1, hitboxe.x2, hitboxe.y2, 10)
+
+spr(108,0,112,16,5)
+spr(108,16,112,16,5)
+spr(108,32,112,16,5)
+spr(108,48,112,16,5)
+spr(108,64,112,16,5)
+spr(108,80,112,16,5)
+spr(108,96,112,16,5)
+spr(108,112,112,16,5)
 end
