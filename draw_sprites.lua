e04fde0171a31a7ea7b72934890eb7ad7ea51f15
Dennborg <mullerdennis1208@gmail.com>
2022-09-18 13:22:49 +0200

    super_pong.p8 -> sprites + draw_sprites.lua -> changed size of sprites

--- draw_sprites.lua
+++ draw_sprites.lua
@@ -1,6 +1,6 @@
 function dsprite()
-  sspr(8,0,2,8,player.x, player.y)
-  sspr(16,0,2,8,126, player.y)
+  sspr(8,0,2,14,player.x, player.y)
+  sspr(16,0,2,14,126, player.y)
   spr(3,ball.x,ball.y)
   //rect(hitboxp.x1, hitboxp.y1, hitboxp.x2, hitboxp.y2, 3)
   //rect(hitboxe.x1, hitboxe.y1, hitboxe.x2, hitboxe.y2, 10)
