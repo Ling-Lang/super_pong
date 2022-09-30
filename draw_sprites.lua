3852d583935c2326a484a60176070b34ff541e24
Dylan <77236413+Ling-Lang@users.noreply.github.com>
2022-09-30 20:35:41 +0200

    *.lua & *.p8 -> Changed something

--- draw_sprites.lua
+++ draw_sprites.lua
@@ -1,8 +1,6 @@
 --Hello
 function dsprite()
-sspr(8,0,2,14,player.x, player.y)
-sspr(16,0,2,14,126, enemy.y)
-  spr(3,ball.x,ball.y)
+
 --  rect(hitboxp.x1, hitboxp.y1, hitboxp.x2, hitboxp.y2, 3)
 --  rect(hitboxe.x1, hitboxe.y1, hitboxe.x2, hitboxe.y2, 10)
 spr(108,0,112,16,5)
@@ -13,4 +11,7 @@ spr(108,64,112,16,5)
 spr(108,80,112,16,5)
 spr(108,96,112,16,5)
 spr(108,112,112,16,5)
+sspr(8,0,2,14,player.x, player.y)
+sspr(16,0,2,14,126, enemy.y)
+spr(5,ball.x,ball.y)
 end
